c5ad412515aed0aac7d0972d5731643b08030aba
Dmitrii Tourkin <112641443+dmitriitourkin@users.noreply.github.com>
2023-11-15 01:11:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 function FilmListing() {
   const films = [
     {id: 1, rating: 3},
@@ -8,42 +9,37 @@ function FilmListing() {
     {id: 6, rating: 2},
     {id: 7, rating: 6},
   ];
+    //Желательно проверку провести в самом начале, а не в компоненте Stars.
+    // Однако проверка на тип и соответствие диапазону рейтинга в компоненте тоже написана
+  const ratedFilms = films.filter(film => {return typeof film.rating !== 'string' && film.rating < 6 && film.rating > 0});
 
   return (
       <div className="films-block">
-          {films.map((film) => (
-              <div id={film.id} className="film-card">
-                  <h4>Название фильма</h4>
-                  <ul className="card-body-stars u-clearfix">
-                    <Stars rating={film.rating}/>
-                  </ul>
+          {ratedFilms.map((film) => (
+              <div className="film-card">
+                  <div className="film-info">
+                      <h4>Название фильма</h4>
+                      <ul className="card-body-stars u-clearfix">
+                        <Stars key={film.id.toString()} count={film.rating}/>
+                      </ul>
+                      <button className='btn btn-dark'>Купить</button>
+                      <button className='btn '>Подробнее</button>
+                  </div>
               </div>
           ))}
       </div>
   );
-}
-function Star() {
-    return (
-        <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
-            <path d="M0 0h18v18H0z" fill="none"/>
-        </svg>
-    );
-}
+};
 
 function Stars(props) {
-  const count = props.rating;
+  const count = props.count;
 
   if (count < 1 || count > 5 || typeof count === 'string') {
     return 'Рейтинг не найден';
-  } 
-
-  const listOfStars = [];
-  for (let i = 0; i < count; i++) {
-    listOfStars.push(<Star key={i}/>)
   };
 
-  console.log(listOfStars)
+  const listOfStars = [];
+  for (let i = 0; i < count; i++) {listOfStars.push(<Star key={i}/>)};
 
   return (
       <div className="stars-ranking">
@@ -53,15 +49,25 @@ function Stars(props) {
               </li>
           </ul>
       </div>
-  )
-}
+  );
+};
+
+function Star() {
+    return (
+        <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
+            <path d="M0 0h18v18H0z" fill="none"/>
+        </svg>
+    );
+};
+
+
 
 function App() {
   return (
       <div className="films-showing">
         <FilmListing/>
       </div>
-  )
-}
-
+  );
+};
 export default App;
